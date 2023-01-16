43298e13c11cc4990e9cde2a3b887eab5af88c94
AndreiTotoro <AndreiTotoro@gmail.com>
2023-01-16 08:07:22 +0200

    Set up inter font

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+import { ChakraProvider } from "@chakra-ui/react";
+import { Inter } from "@next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={inter.className}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </main>
+  );
 }
